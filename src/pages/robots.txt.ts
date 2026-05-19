17f207ff5a4cfc0c707a42aaee5a96c983275c03
nearmesingapore <nearmesingapore@gmail.com>
2026-05-20 06:04:13 +0800

    Use static root robots file

--- src/pages/robots.txt.ts
+++
@@ -1,9 +0,0 @@
-import { buildRobotsTxt } from "@/lib/seo";
-
-export function GET({ site }: { site: URL | undefined }) {
-  return new Response(buildRobotsTxt(site), {
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8"
-    }
-  });
-}
